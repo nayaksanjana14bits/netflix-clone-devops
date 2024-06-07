77c2378efe15de4eddbf937a6d31534d7a98d96a
nayaksanjana14bits <2023sl93005@wilp.bits-pilani.ac.in>
2024-06-07 14:20:20 +0530

    lazy_loading

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -1,29 +1,32 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';  
-import Netflix from './pages/Netflix';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Player from './pages/Player';
-import Movies from './pages/Movies';
-import TVShows from './pages/TVShow';
-import UserListedMovies from './pages/UserListedMovies';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+// Lazy load the components
+const Netflix = lazy(() => import('./pages/Netflix'));
+const Login = lazy(() => import('./pages/Login'));
+const Signup = lazy(() => import('./pages/Signup'));
+const Player = lazy(() => import('./pages/Player'));
+const Movies = lazy(() => import('./pages/Movies'));
+const TVShows = lazy(() => import('./pages/TVShow')); // Fixed import path
+const UserListedMovies = lazy(() => import('./pages/UserListedMovies'));
 
 const App = () => {
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path="/login" element={<Login />} />
-    {/* <Route path="/signup" element={<Signup />} /> */}
-    <Route path="/player" element={<Player />} />
-    <Route path="/movies" element={<Movies />} />
-    <Route path="/tv" element={<TVShows />} />
-    <Route path="/mylist" element={<UserListedMovies />} />
-    <Route path="/home" element={<Netflix />} />
-    <Route path="/" element={<Signup />} />
-    
-   </Routes>   
-  </BrowserRouter> );
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/tv" element={<TVShows />} />
+          <Route path="/mylist" element={<UserListedMovies />} />
+          <Route path="/home" element={<Netflix />} />
+          <Route path="/" element={<Signup />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ netflix-ui/src/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+  },
+};
